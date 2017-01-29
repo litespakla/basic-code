30a7ea03b0b8a5892391d0723535e9595af97d72
litespakla <sebas.9624@gmail.com>
2017-01-28 21:03:51 -0600

    Solve quadratic equations
    Get the real solutions of a quadratic equation

---
+++ C/quadratic_equation.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int quadratic (double a, double b, double c);
+
+int solution(){
+double a;
+double b;
+double c;
+printf("The equation is Ax^2 + Bx + C\n\nEnter the coefficient A of the equation: ");
+scanf("%lf", &a);
+printf("\nEnter the coefficient B of the equation: ");
+scanf("%lf", &b);
+printf("\nEnter the coefficient C of the equation: ");
+scanf("%lf", &c);
+quadratic(a, b, c);
+return 0;
+}
+
+int quadratic (double a, double b, double c){
+double d;
+double e;
+d=pow(b,2)-4*a*c;
+if (d<0){
+printf("\nEquation has no real roots\n");
+}
+
+else{
+    e=(-b+sqrt(d))/(2*a);
+    d=(-b-sqrt(d))/(2*a);
+    printf("\nThe roots of the quadratic equation are:\nX1 = %g\nX2 = %g\n", e, d);
+}
+return 0;
+}
+
+int main (void){
+solution();
+return 0;
+}
