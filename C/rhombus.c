85c47cd44623a01cf4a50c75893130937821601a
litespakla <sebas.9624@gmail.com>
2017-01-28 21:06:22 -0600

    Print a rhombus of asterisks of size N

---
+++ C/rhombus.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int static spaces (int n, int N){
+  int k;
+  for (k=1; k<=N-n; k++){
+    printf(" ");
+  }
+  return 0;
+}
+
+int static rhombus (int N){
+int i;
+int j;
+for (i=1; i<=N; i++){
+  spaces(i, N);
+  for (j=1; j<=i; j++){
+    printf("* ");
+  }
+  printf("\n");
+}
+
+for (i=N-1; 1<=i; i=i-1){
+  spaces(i, N);
+  for (j=i; 1<=j; j=j-1){
+    printf("* ");
+  }
+  printf("\n");
+}
+return 0;
+}
+
+int main(int argc, char *argv[]) {
+rhombus(atoi(argv[1]));
+  return 0;
+}
