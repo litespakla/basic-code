2d3e9a04833a6b54d1ae2ee6e82d84084cb99ae2
Juan Sebastian Pereira Cubillo <sebas.9624@gmail.com>
2017-02-12 14:15:08 -0600

    Very inefficient way to approximate Pi

--- C/pi_with_mandelbrot.c
+++ C/pi_with_mandelbrot.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-double funcion (double z, double e){
+/*Based on this video: https://www.youtube.com/watch?v=d0vY0CKYhPY&list=PLt5AfwLFPxWJ8GCgpFo5_OSyfl7j0nOiu&index=3 */
+
+double funcion (double z, double e){ //Function seen in the video
 double c=0.25+e;
 double x=pow(z,2)+c;
 return x;
@@ -14,13 +16,13 @@ printf("Enter a very tiny value: ");
 scanf("%lf", &e);
 double a=0.0;
 double m=0.0;
-while(m<=2){
+while(m<=2){ //Number of steps the algorithm lasts to surpass 2
        m=funcion(m,e);
        a=a+1;
-       }
-       while (10<a) {
-         a=a/10;
-              }
+}
+while (10<a) { //Divides the number to make it less than 10
+      a=a/10;
+}
          printf("Pi is %g aproximately \n",a);
 return 0;
 }
