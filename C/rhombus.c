988049b417778489e7b24b03e96a174110378c84
Juan Sebastian Pereira Cubillo <sebas.9624@gmail.com>
2017-02-12 14:31:01 -0600

    Prints a rhombus of asterisks of size N

--- C/rhombus.c
+++ C/rhombus.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-int static spaces (int n, int N){
+int static spaces (int n, int N){ //Prints the required amount of spaces for each file
   int k;
   for (k=1; k<=N-n; k++){
     printf(" ");
@@ -13,7 +13,10 @@ int static spaces (int n, int N){
 int static rhombus (int N){
 int i;
 int j;
-for (i=1; i<=N; i++){
+/*int N;
+printf("Size of the rhombus?: ");
+scanf("%i", &N);*/
+for (i=1; i<=N; i++){ //Prints the upper part of the rhombus
   spaces(i, N);
   for (j=1; j<=i; j++){
     printf("* ");
@@ -21,7 +24,7 @@ for (i=1; i<=N; i++){
   printf("\n");
 }
 
-for (i=N-1; 1<=i; i=i-1){
+for (i=N-1; 1<=i; i=i-1){ //Prints the bottom part of the rhombus
   spaces(i, N);
   for (j=i; 1<=j; j=j-1){
     printf("* ");
